97d0847946922ddfce07e16e95a26029fe03f9de
Li Ge <lge@ucsd.edu>
2015-01-27 09:54:55 -0800

    fix template

--- tools/templates/test.js
+++ tools/templates/test.js
@@ -3,7 +3,7 @@ var ROOT = __dirname + '/../..', SRC = ROOT + '/src';
 var expect = require('expect.js');
 require(SRC + '/<%= name %>.js');
 
-describe('<% name =>', function() {
+describe('<%= name %>', function() {
   it('should fail', function() {
     expect(false).to.be(true);
   });
