e0b7e7f2e56b496fe23a25dd957948d84f1550fd
Li Ge <lge@ucsd.edu>
2015-02-16 23:40:41 -0800

    update test

--- test/spec/planar_truss_2d.js
+++ test/spec/planar_truss_2d.js
@@ -6,19 +6,102 @@ var fe = require(SRC);
 
 describe('FAESOR Planar_truess_with_anim example', function() {
 
-  xit('should have same nodal displacements', function() {
-    var creator = new fe.geometry.HypercubeGeometryCreator();
-    var n1 = creator.point([0, 0]);
-    var n2 = creator.point([0, 40]);
-    var n3 = creator.point([40, 0]);
-    var n4 = creator.point([40, 40]);
-    var n5 = creator.point([80, 0]);
-    var n6 = creator.point([80, 40]);
+  it('should create model.', function() {
+    var E = 1e7;
+    var integrationOrder = 1;
 
-    var l1 = creator.line(n1, n3);
-    var l2 = creator.line(n1, n4);
-    var l3 = creator.line(n2, n4);
+    var fens = new fe.geometry.pointset([
+      [0, 0]
+      [0, 40]
+      [40, 0]
+      [40, 40]
+      [80, 0]
+      [80, 40]
+    ]);
 
+    var gcells = new fe.geometery.gcellset.L2({
+      conn: [
+        [ 1, 3 ],
+        [ 1, 4 ],
+        [ 2, 4 ],
+        [ 3, 4 ],
+        [ 3, 5 ],
+        [ 5, 4 ],
+        [ 6, 4 ],
+        [ 5, 6 ]
+      ],
+      otherDimension: 1.5
+    });
+
+    var ebcFenids =[ 1, 1, 2, 2 ];
+    var ebcPrescribed = [ 1, 1, 1, 1 ];
+    var ebcComp = [ 1, 2, 1, 2 ];
+    var ebcVal = ebc_comp.map(function() { return 0; });
+
+    var prop = new fe.property.LinearElasticISO({ E: E });
+    var mater = new fe.material.DeformableSmallStrainLinearlyElasticUniaxial({
+      property: prop
+    });
+
+    var feb = new fe.feblock.DeformableSmallStrain({
+      material: mater,
+      gcells: gcells,
+      integrationRule: new fe.numeric.GaussRule(1, integrationOrder),
+      Rm: fe.utils.genISORm
+    });
+
+    var geomField = new fe.field.Field({
+      name: 'geom',
+      dim: 2,
+      fens: fens
+    });
+
+    var u = geomField.clone();
+    u.name = 'u';
+    u = u.map(function() { return 0; });
+
+    u = u.setEBC(ebcFenids, ebcPrescribed, ebcComp, ebcVal);
+    u = u.applyEBC();
+
+    u = u.numberEquations();
+
+    var ems = feb.stiffness(geomField, u);
+    var neqns = u.getNumEquations();
+    var K = new fe.numeric.DokSparseMatrix([], neqns, neqns);
+    fe.assemble.assemble_(K, ems);
+
+    var F = new fe.numeric.SparseVector([], neqns);
+    var nodalLoads = [
+      { nodeId: 3, dir: 2, magnitude: -2000 },
+      { nodeId: 5, dir: 1, magnitude: +2000 },
+      { nodeId: 6, dir: 1, magnitude: +4000 },
+      { nodeId: 6, dir: 2, magnitude: +6000 },
+    ].map(function(item) {
+      return new fe.nodalload.NodalLoad(item);
+    });
+
+    nodalLoads.forEach(function(nl) {
+      var evs = nl.loads(u);
+      fe.assemble.assembleF_(F, evs);
+    });
+
+    var x = fe.numeric.mldivide(K, F);
+
+    u = u.scatter(x);
+
+    var values = u.getValues();
+    console.log("values = ", values);
+
+    var expected = [
+      [0, 0],
+      [0, 0],
+      [0.0213, 0.0408],
+      [-0.0160, 0.0462],
+      [0.0427, 0.1501],
+      [-0.0053, 0.1661],
+    ];
+
+    expect(fe.numeric.vecEqual(values, expected)).to.be(true);
   });
 
 });
