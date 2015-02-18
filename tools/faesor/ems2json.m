df87415a79e526523d955c75b5e207b3f3e7cff5
Li Ge <lge@ucsd.edu>
2015-02-17 22:44:24 -0800

    update tools

--- tools/faesor/ems2json.m
+++ tools/faesor/ems2json.m
@@ -7,21 +7,21 @@ N = length(mats);
 for i = 1:N
     mat = mats{i};
     eqn = eqns{i};
-    
+
     if i > 1
         json = strcat(json, ',\n');
     end
-    json = strcat(json, '[\n');
-    
-    json = strcat(json, '{\n"matrix":\n');
-    json = strcat(json, mat2json(mat));
-    json = strcat(json, '},\n');
-    
-    json = strcat(json, '{\n"equationNumbers":\n');
-    json = strcat(json, vec2json(eqn), '\n');
-    json = strcat(json, '}\n');
-    
-    json = strcat(json, ']');    
+
+    json = strcat(json, '{\n');
+
+    json = strcat(json, '"matrix":\n');
+    json = strcat(json, mat2json(mat), ',\n');
+
+    json = strcat(json, '"equationNumbers":\n');
+    json = strcat(json, vec2json(eqn));
+
+    json = strcat(json, '\n}');
+
 end
 
 json = strcat(json, '\n]\n');
