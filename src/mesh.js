c420755593153c8d95cbb202997a3e93d6838a26
Li Ge <lge@ucsd.edu>
2015-03-17 18:03:23 -0700

    H8Block

--- src/mesh.js
+++ src/mesh.js
@@ -1,15 +1,21 @@
 /*global require*/
-// mesh
+// dependencies
 var _ = require('./core.utils');
 var check = _.check;
 var isObject = check.object;
+var array2d = _.array2d;
 var fens = require('./fens');
 var FeNodeSet = fens.FeNodeSet;
 var gcells = require('./gcellset');
 var Q4 = gcells.Q4;
+var H8 = gcells.H8;
 
 /**
- * @typedef Mesh~InitOption
+ * @module mesh
+ */
+
+/**
+ * @typedef module:mesh.MeshInitOption
  * @property {FeNodeSet} fens - optional. finite element node set.
  * @property {Array} xyz - optional. 2D array of node coordinates.
  * @property {GCellSet} gcells - optional. geometry cell set.
@@ -20,7 +26,7 @@ var Q4 = gcells.Q4;
 /**
  * Mesh class.
  * @class
- * @param {Mesh~InitOption} options
+ * @param {module:mesh.MeshInitOption} options
  * @example
 
    var msh1 = new Mesh({
@@ -43,21 +49,21 @@ function Mesh(options) {
 
 /**
  * Returns finite element node set of the mesh.
- * @returns {FeNodeSet}
+ * @returns {module:fens.FeNodeSet}
  */
 Mesh.prototype.fens = function() { return this._fens; };
 
 /**
  * Returns geometry cell set of the mesh.
- * @returns {GCellSet}
+ * @returns {module:gcellset.GCellSet}
  */
 Mesh.prototype.gcells = function() { return this._gcells; };
 
 /**
  * Creates a L-shaped domain using 3 quads.
- * @returns {Mesh}
+ * @returns {module:mesh.Mesh}
  */
-function L2x2() {
+exports.L2x2 = function L2x2() {
   var fens, gcells;
   fens = new FeNodeSet({
     xyz: [
@@ -81,6 +87,60 @@ function L2x2() {
   });
 
   return new Mesh({ fens: fens, gcells: gcells });
-}
+};
+var L2x2 = exports.L2x2;
 
-exports.L2x2 = L2x2;
+/**
+ * Creates a H8 block mesh.
+ * @param {Number} w - width in x direction.
+ * @param {Number} l - length in y direction.
+ * @param {Number} h - height in z direction.
+ * @param {Int} nx - number of divisions in x direction.
+ * @param {Int} ny - number of divisions in y direction.
+ * @param {Int} nz - number of divisions in z direction.
+ * @returns {module:mesh.Mesh}
+ */
+exports.H8Block = function(w, l, h, nx, ny, nz) {
+  var dx = w/nx, dy = l/ny, dz = h/nz;
+  var nn = (nx+1)*(ny+1)*(nz+1);
+  var xyz = new Array(nn);
+  var conn = new Array(nx*ny*nz);
+  var i, j, k;
+
+  // return 1d index from 3 index
+  function ijkToIndex(i, j, k, ni, nj, nk) {
+    return i*nj*nk + j*nk + k;
+  }
+
+  // nodes
+  for (i = 0; i < nx + 1; ++i)
+    for (j = 0; j < ny + 1; ++j)
+      for (k = 0; k < nz + 1; ++k)
+        xyz[ijkToIndex(i, j, k, nx+1, ny+1, nz+1)] = [i*dx, j*dy, k*dz];
+
+  // connectivity:
+  for (i = 0; i < nx; ++i)
+    for (j = 0; j < ny; ++j)
+      for (k = 0; k < nz; ++k)
+        conn[ijkToIndex(i, j, k, nx, ny, nz)] = cellConnAt(i, j, k);
+
+  function cellConnAt(i, j, k) {
+    return [
+      ijkToIndex(i, j, k, nx+1, ny+1, nz+1),
+      ijkToIndex(i, j, k+1, nx+1, ny+1, nz+1),
+      ijkToIndex(i, j+1, k+1, nx+1, ny+1, nz+1),
+      ijkToIndex(i, j+1, k, nx+1, ny+1, nz+1),
+
+      ijkToIndex(i+1, j, k, nx+1, ny+1, nz+1),
+      ijkToIndex(i+1, j, k+1, nx+1, ny+1, nz+1),
+      ijkToIndex(i+1, j+1, k+1, nx+1, ny+1, nz+1),
+      ijkToIndex(i+1, j+1, k, nx+1, ny+1, nz+1)
+    ];
+  }
+
+  var fens = new FeNodeSet({ xyz: xyz });
+  var gcells = new H8({ conn: conn });
+  var mesh = new Mesh({ fens: fens, gcells: gcells });
+
+  return mesh;
+};
--- test/unit/mesh.test.js
+++ test/unit/mesh.test.js
@@ -1,10 +1,64 @@
 /*global __dirname describe it require*/
 var ROOT = __dirname + '/../..', SRC = ROOT + '/src';
 var expect = require('expect.js');
-require(SRC + '/mesh.js');
+var dataDriven = require('data-driven');
+var mesh = require(SRC + '/mesh.js');
 
 describe('mesh', function() {
-  xit('should fail', function() {
-    expect(false).to.be(true);
+  describe('H8Block', function() {
+    var H8Block = mesh.H8Block;
+    var dataset = [
+      {
+        w: 1, l: 1, h: 1,
+        nx: 1, ny: 1, nz: 1,
+        desc: 'single block',
+        expectedXYZ: [
+          [ 0, 0, 0 ],
+          [ 0, 0, 1 ],
+          [ 0, 1, 0 ],
+          [ 0, 1, 1 ],
+          [ 1, 0, 0 ],
+          [ 1, 0, 1 ],
+          [ 1, 1, 0 ],
+          [ 1, 1, 1 ]
+        ],
+        expectedConn: [
+          [ 0, 1, 3, 2, 4, 5, 7, 6 ]
+        ]
+      },
+      {
+        w: 1, l: 1, h: 1,
+        nx: 1, ny: 1, nz: 2,
+        desc: 'two block',
+        expectedXYZ: [
+          [ 0, 0, 0 ],
+          [ 0, 0, 0.5 ],
+          [ 0, 0, 1 ],
+          [ 0, 1, 0 ],
+          [ 0, 1, 0.5 ],
+          [ 0, 1, 1 ],
+          [ 1, 0, 0 ],
+          [ 1, 0, 0.5 ],
+          [ 1, 0, 1 ],
+          [ 1, 1, 0 ],
+          [ 1, 1, 0.5 ],
+          [ 1, 1, 1 ]
+        ],
+        expectedConn: [
+          [ 0, 1, 4, 3, 6, 7, 10, 9 ],
+          [ 1, 2, 5, 4, 7, 8, 11, 10 ]
+        ]
+      }
+    ];
+
+    dataDriven(dataset, function() {
+      it('should work {desc}', function(ctx) {
+        var m = H8Block(ctx.w, ctx.l, ctx.h, ctx.nx, ctx.ny, ctx.nz);
+        var xyz = m.fens().xyz();
+        var conn = m.gcells().conn();
+        expect(xyz).to.eql(ctx.expectedXYZ);
+        expect(conn).to.eql(ctx.expectedConn);
+      });
+    });
   });
 });
